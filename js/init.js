8370ba7646eb1b73da6d92c610a84be5164e8eb8
Xuning Yang <xuning.y@gmail.com>
2016-12-26 19:19:14 +0800

    update layout

--- js/init.js
+++ js/init.js
@@ -33,7 +33,7 @@
 					'wide': { range: '-1680', containers: 960 },
 					'normal': { range: '-1080', containers: '95%' },
 					'narrow': { range: '-840', containers: '95%', grid: { gutters: 30 } },
-					'mobile': { range: '-640', lockViewport: true, containers: '95%', grid: { collapse: true, gutters: 20 } }
+					'mobile': { range: '-600', lockViewport: true, containers: '95%', grid: { collapse: true, gutters: 20 } }
 				}
 			},
 
